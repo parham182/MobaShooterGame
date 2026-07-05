a0eabd2a5db1f3ad63fb3abaf162c1a7b3aa90ac
DevMood <parhamqop@gmail.com>
2026-07-06 00:09:11 +0330

    Animations and gun System

--- MobaShooterGame/Assets/Scripts/Creep.cs
+++ MobaShooterGame/Assets/Scripts/Creep.cs
@@ -1,5 +1,6 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Creep : NetworkBehaviour, IDamageable
 {
@@ -15,18 +16,33 @@ public class Creep : NetworkBehaviour, IDamageable
     [SerializeField] private float towerDamageMultiplier;
     [SerializeField] private Transform firePoint;
 
+    [Header("Creep Movement")]
+    [SerializeField] Transform moveTargetBlueTeam;
+    [SerializeField] Transform moveTargetRedTeam;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] Animator animator;
+    NavMeshAgent navMeshAgent;
+
     private IDamageable target;
     private float timer = 0;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
 
     [ServerCallback]
     private void Update()
     {
+        creepMovement();
+
         timer += Time.deltaTime;
         // find target
         float closestTargetDistance = float.MaxValue;
         target = null;
 
-        foreach(IDamageable t in SpawnManager.instance.targets)
+        foreach (IDamageable t in SpawnManager.instance.targets)
         {
             if (t.DamageableSide() != creepSide)
             {
@@ -59,13 +75,36 @@ public class Creep : NetworkBehaviour, IDamageable
                                 : damage;
 
                             dmg.TakeDamage(finalDamage);
+                            moveSpeed = 0;
                         }
                     }
                 }
-            } else // go closer (target its not in attack range)
-            {
-                
             }
+            else // go closer (target its not in attack range)
+            {
+                moveSpeed = 10;
+            }
+        }
+    }
+
+    private void creepMovement()
+    {
+        navMeshAgent.speed = moveSpeed;
+        if (creepSide == "blue")
+        {
+            navMeshAgent.SetDestination(moveTargetBlueTeam.transform.position);
+        }
+        else if (creepSide == "red")
+        {
+            navMeshAgent.SetDestination(moveTargetRedTeam.transform.position);
+        }
+        if (moveSpeed > 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
         }
     }
 
