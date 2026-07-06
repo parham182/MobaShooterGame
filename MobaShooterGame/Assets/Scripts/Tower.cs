71ab58a83eed70f16b7a7353934e2a90ab15e9cc
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2026-07-06 23:19:16 +0330

    improving tower AI

--- MobaShooterGame/Assets/Scripts/Tower.cs
+++ MobaShooterGame/Assets/Scripts/Tower.cs
@@ -48,33 +48,26 @@ public class Tower : NetworkBehaviour, IDamageable
             }
         }
 
-        if (target != null && timer >= attackInterval)
+        if (target != null)
         {
-            timer = 0;
-            float distance = Vector3.Distance(transform.position, target.GetPosision());
-            if (distance <= attackRange) // attack the target
+            Vector3 dir = target.GetPosision() - TowerGun.transform.position;
+            dir.y = 0f;
+
+            if (dir != Vector3.zero) TowerGun.transform.rotation =
+                Quaternion.LookRotation(dir) * Quaternion.Euler(0, 90f, 0);
+
+            if (timer >= attackInterval)
             {
-                Vector3 dir = target.GetPosision() - TowerGun.transform.position;
-                dir.y = 0f;
-
-                if (dir != Vector3.zero)
+                timer = 0;
+                float distance = Vector3.Distance(transform.position, target.GetPosision());
+                if (distance <= attackRange) // attack the target
                 {
-                    TowerGun.transform.rotation =
-                        Quaternion.LookRotation(dir) * Quaternion.Euler(0, 90f, 0);
-                }
-                Vector3 rayDir = Quaternion.Euler(0, -90f, 0) * TowerGun.transform.forward;
-
-                if (Physics.Raycast(attackPoint.position, rayDir, out RaycastHit hit))
-                {
-                    if (hit.collider.TryGetComponent(out IDamageable dmg))
-                    {
-                        float finalDamage = 
-                            dmg.GetDamageableType() == damageableType.Player
-                            ? damage * playerDamageMultiplier
-                            : damage;
-                        
-                        dmg.TakeDamage(finalDamage, damageableType.Building, towerSide);
-                    }
+                    float finalDamage = 
+                        target.GetDamageableType() == damageableType.Player
+                        ? damage * playerDamageMultiplier
+                        : damage;
+                            
+                    target.TakeDamage(finalDamage, damageableType.Building, towerSide);
                 }
             }
         }
